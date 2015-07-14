2ce97ea2d06d6843f7b06446d477b54f11925318
Nishanth <childi.nishanth@gmail.com>
2015-07-14 11:09:40 +0530

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,10 +27,43 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/achievments', {
+        templateUrl: 'views/achievments.html',
+        controller: 'AboutCtrl'
+      })
       .when('/team', {
         templateUrl: 'views/team.html',
         controller: 'AboutCtrl'
       })
+      .when('/parisutham', {
+        templateUrl: 'views/parisutham.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/dhanyam', {
+        templateUrl: 'views/dhanyam.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/ayurekha', {
+        templateUrl: 'views/ayurekha.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/swatch', {
+        templateUrl: 'views/swatch.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/krishi', {
+        templateUrl: 'views/krishi.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'AboutCtrl'
+      })
+
+
+
+
+
       .otherwise({
         redirectTo: '/'
       });
